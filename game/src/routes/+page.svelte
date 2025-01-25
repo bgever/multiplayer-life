185cd94d038e073f1760b7cd0321e4d7c8628902
Bart Verkoeijen <bart.verkoeijen@gmail.com>
2025-01-25 09:53:29 +0800

    Game pages

--- game/src/routes/+page.svelte
+++ game/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Conway’s Game of Life: Multiplayer</h1>
+<p><a href="/game">Join Game</a></p>
+<p><a href="/arena">6 Games Arena</a></p>
---
+++ game/src/routes/arena/+page.svelte
@@ -0,0 +1,5 @@
+<main class="grid grid-cols-3 gap-4 p-4 h-screen">
+	{#each { length: 6 }, rank}
+		<iframe src="/game" title="Game {rank + 1}" class="border border-indigo-600 w-full h-full"></iframe>
+	{/each}
+</main>
