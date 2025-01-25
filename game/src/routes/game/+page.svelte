956947f719fee20ecac3a2b5c3ed2ebbf21be42b
Bart Verkoeijen <bart.verkoeijen@gmail.com>
2025-01-25 10:42:09 +0800

    Basic WebSocket message exchange

--- game/src/routes/game/+page.svelte
+++ game/src/routes/game/+page.svelte
@@ -1,1 +1,16 @@
+<script lang="ts">
+	import type {ClientSideEvents, ServerSideEvents} from '$lib/engine/server';
+	import type {Socket} from 'socket.io';
+	import {io} from 'socket.io-client';
+
+	let currentTime = $state(0);
+
+	const socket = io() as unknown as Socket<ServerSideEvents, ClientSideEvents>;
+	socket.on('hello', time => {
+		currentTime = time;
+	});
+</script>
+
 <h1>Game</h1>
+
+<p>Message: {new Date(currentTime)}</p>
