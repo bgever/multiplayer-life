87d3c29a6bbffe1fcd10a6e2e83f56563ff49a7d
Bart Verkoeijen <bart.verkoeijen@gmail.com>
2025-01-26 12:38:52 +0800

    Test rendering Svelte grid

---
+++ game/src/lib/components/Grid.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import GridContainer from './GridContainer.svelte';
+	import {cell} from './GridCell.svelte';
+	import type {GridData} from '$lib/types/grid';
+
+	type Props = {
+		data: GridData;
+	};
+
+	const {data}: Props = $props();
+</script>
+
+<GridContainer {data} {cell} />
---
+++ game/src/lib/components/GridContainer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type {Snippet} from 'svelte';
+	import type {GridData} from '$lib/types/grid';
+
+	type Props = {
+		data: GridData;
+		cell: Snippet<[string | null]>;
+	};
+
+	const {data, cell}: Props = $props();
+</script>
+
+<div class="grid grow" style:grid-template-columns="repeat({data.length}, 1fr)">
+	{#each data as cells, y}
+		{#each cells as { color }, x}
+			{@render cell(color)}
+		{/each}
+	{/each}
+</div>
